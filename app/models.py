c31ba0847c5a60f24c6d87026872197bf234c34d
Robert Maina <robert.maina@student.moringaschool.com>
2021-08-15 06:02:31 +0300

    added two classes for user model and pitches model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,24 @@
 from . import db
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    
+    
 
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+        
     def __repr__(self):
-        return f'User {self.username}'
+        return f'Pitch {self.post}'
