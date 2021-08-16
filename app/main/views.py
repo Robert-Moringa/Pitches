e60989878b2a57e858b7927e93e7e57fb1415914
Robert Maina <robert.maina@student.moringaschool.com>
2021-08-16 22:11:14 +0300

    added uploading image functionality

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for,abort,request
 from . import main
-from .. import db
+from .. import db, photos
 from ..models import User,Pitch,Comment,Upvote,Downvote
 from flask_login import login_required, current_user
 from .form import UpdateProfile,PitchForm,CommentForm
@@ -48,3 +48,14 @@ def updateprofile(name):
         user.save_u()
         return redirect(url_for('.profile',name = name))
     return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
